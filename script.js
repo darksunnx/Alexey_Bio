00d2d127873cb416369a09a21fcf98386d161be3
Alexandr Osintov <vavilanchikomarchik@gmail.com>
2025-03-27 21:22:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Function to load Telegram profile picture
     function loadTelegramProfilePicture() {
         // Using telegram's direct CDN URL for the avatar
-        const avatarUrl = `https://t.me/i/userpic/320/${username}.jpg`;
+        const avatarUrl = `https://cdn4.cdn-telegram.org/file/kYNZBFzJIvrQgMoeS0jUh9-qxZY_6BcB0YMWUNQbP7Zhl9zRjYt8rFqHwFLUGBJRGE4cYC30hqSnCU8W6lptY1iltweMwdM5vbj7NAldTsgZz3a1lOozMPXfHaLmZIMsrSPaK1SJPdxdN3ZylQz0Ex492l3l1KPONrHCzWakfXZmjRgAPQGFlnGiximuYxn1WPVc6anAHVChvvDpXYesQzLknUUfsvjQk56PViwaI_2Nz0lNw3d3cMRtAaXVkJ3ETV3TLmk0uZP4fdCMkxqvAxINJVXLPhvt6m5WmDWmNY24skVd-bsp9HXCb-4QCxCbuk_lbUUs09Pqus0CB7t6Sw.jpg`;
 
         avatarImg.src = avatarUrl;
 
